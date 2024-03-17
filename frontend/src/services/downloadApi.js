8bcd786d1f865ffbb8edc0792ffb8e522da0e3b2
Kanchana224 <kkanchana427@gmail.com>
2024-03-17 12:41:13 +0530

    updated

--- frontend/src/services/downloadApi.js
+++ frontend/src/services/downloadApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://filesshare-a6te.onrender.com';
 // const API_URL = process.env.API_URL;
 
 export const downloadFile = async (enteredCode , code) => {
