1253229fffcda0524d8db9789e5f8b71c47e722c
Kanchana224 <kkanchana427@gmail.com>
2024-03-18 10:27:44 +0530

    updated

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,14 +1,14 @@
 //handels all the routes of frontend
 
 import express from 'express';
-import { uploadImage , getImage, downloadfile} from '../controller/imageController.js';
+import { uploadImage , getImage, downloadFile} from '../controller/imageController.js';
 import { getCode } from '../controller/getCode.js';
 import upload from '../utils/uplode.js';
 
 const router = express.Router();
 
 router.post('/upload', upload.single('file'), uploadImage);
-router.get('/file/:_id', downloadfile);
+router.get('/file/:_id', downloadFile);
 router.get('/download/:code', getImage);
 router.get('/getDBcode/:code', getCode);
 
